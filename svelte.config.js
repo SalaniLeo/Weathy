5994c1501fb9a5a5e79a53d0ed249ca91695b345
SalaniLeo <97695689+SalaniLeo@users.noreply.github.com>
2023-11-05 11:51:18 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '/static/404.html'
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
 };
