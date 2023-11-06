5063b4c41ec1626a9036c9f0c16ffc3b0498e204
SalaniLeo <97695689+SalaniLeo@users.noreply.github.com>
2023-11-06 17:06:34 +0100

    set fetch api to onMount()

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,49 +4,46 @@ function fetchApi() {
 	return new Promise((resolve) => {
 		resolve(fetch('http://api.openweathermap.org/data/2.5/forecast?lat=44.8268&lon=11.6207&units=metric&appid=72d251b81d30ef572ae667dfe6c4ee1a')
         .then((response)=>response.json())
-        .then((responseJson)=>{return responseJson}));
-		console.log("Called")
+        .then((responseJson)=>{
+
+		let response = responseJson
+
+		let minTemp = response.list[0]['main']['temp']
+		let maxTemp = response.list[0]['main']['temp']
+		let days = [];
+		let counter = 0;
+
+		for(var i in response.list){
+			let time = response.list[i]['dt_txt'].slice(11, 13)
+			let temperature = Math.round(response.list[i]['main']['temp']*10) / 10
+			let icon = "";
+
+			if(minTemp > temperature){
+				minTemp = temperature
+			} else if (maxTemp < temperature) {
+				maxTemp
+			}
+
+			if(time == 0){
+				let date = response.list[counter]['dt'];
+
+				if(counter-3 < 0){
+					days.push({ name: `${getWeekDays(date)}`, icon: `${mapWeatherIconToName(response.list[0]['weather'][0]['icon'])}`, minTemp: `${minTemp}`, maxTemp: `${maxTemp}`})
+				} else {
+				days.push({ name: `${getWeekDays(date)}`, icon: `${mapWeatherIconToName(response.list[counter-3]['weather'][0]['icon'])}`, minTemp: `${minTemp}`, maxTemp: `${maxTemp}`})
+				}
+
+				minTemp = temperature
+				maxTemp = temperature
+			}
+			counter = counter + 1
+		}
+		return days
+	}));
 	});
   }
 
-  async function getForecast() {
-	const result = await fetchApi();
-
-	let minTemp = result.list[0]['main']['temp']
-	let maxTemp = result.list[0]['main']['temp']
-	let days = [];
-	let counter = 0;
-
-	for(var i in result.list){
-		let time = result.list[i]['dt_txt'].slice(11, 13)
-		let temperature = Math.round(result.list[i]['main']['temp']*10) / 10
-		let icon = "";
-
-		if(minTemp > temperature){
-			minTemp = temperature
-		} else if (maxTemp < temperature) {
-			maxTemp
-		}
-
-		if(time == 0){
-			let date = result.list[counter]['dt'];
-
-			if(counter-3 < 0){
-				days.push({ name: `${getWeekDays(date)}`, icon: `${mapWeatherIconToName(result.list[0]['weather'][0]['icon'])}`, minTemp: `${minTemp}`, maxTemp: `${maxTemp}`})
-			} else {
-			days.push({ name: `${getWeekDays(date)}`, icon: `${mapWeatherIconToName(result.list[counter-3]['weather'][0]['icon'])}`, minTemp: `${minTemp}`, maxTemp: `${maxTemp}`})
-			}
-			console.log(counter-3)
-
-			minTemp = temperature
-			maxTemp = temperature
-		}
-		counter = counter + 1
-	}
-	return days
-  }
-  
-  let weather = getForecast();
+  let weather = fetchApi();
 
   function getWeekDays(unix_timestamp)
   {
@@ -78,7 +75,6 @@ function fetchApi() {
       "50d": "weather-mist.svg",
       "50n": "weather-mist.svg",
     };
-	console.log(icon)
     return iconToName[icon] || "weather-clear.svg";
   }
 
